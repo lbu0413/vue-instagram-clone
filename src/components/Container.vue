56662d3a1970f5f283b397b72da9431f9f8223b2
LBU <noeliden1@gmail.com>
2021-05-05 00:07:08 -0400

    initial commit

---
+++ src/components/Container.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Post />
+    <Post />
+    <Post />
+  </div>
+</template>
+
+<script>
+import Post from "./Post";
+
+export default {
+  name: "Container",
+  components: {
+    Post,
+  },
+};
+</script>
+
+<style></style>
